e9d0625028515e90e42a0b944f4e26b91e3a8478
sunakim <sakim@icomas.co.kr>
2023-01-17 14:44:11 +0900

    update item api

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    /**
+     * 영속성 컨텍스트가 자동 변경
+     */
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {       // bookParam : 파라미터로 넘어온 준영속 상태의 엔티티
+        Item findItem = itemRepository.findOne(itemId);         // 같은 엔티티를 조회
+        findItem.setName(name);                                 // 데이터 수정
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItem() {
